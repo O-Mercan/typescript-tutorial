2714b79746dbf59b261233c7a7b7ffc5dc2e828e
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2024-12-25 00:06:49 +0300

    union types added

--- hello_world/src/index.ts
+++ hello_world/src/index.ts
@@ -57,16 +57,28 @@
 
 
 //Type alias
-type Employee = {
-    readonly id: number,
-    name: string,
-    retire: (date: Date) => void
+// type Employee = {
+//     readonly id: number,
+//     name: string,
+//     retire: (date: Date) => void
+// }
+
+// let employee: Employee = {
+//     id: 1,
+//     name: "Mosh",
+//     retire: (date: Date) => {
+//         console.log(date);
+//     }
+// }
+
+//Union Types
+function kgToLbs(weight: number | string): number {
+    //Narrowing
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+    return parseInt(weight) * 2.2;
 }
 
-let employee: Employee = {
-    id: 1,
-    name: "Mosh",
-    retire: (date: Date) => {
-        console.log(date);
-    }
-}
+kgToLbs(10);
+kgToLbs("10kg");
