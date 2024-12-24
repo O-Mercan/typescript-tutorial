9500c411213a1eacef04735586956218c744cc5a
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2024-12-24 23:54:16 +0300

    objects added

--- hello_world/dist/index.js
+++ hello_world/dist/index.js
@@ -1,14 +1,9 @@
 "use strict";
-const small = 1;
-const medium = 2;
-const large = 3;
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 1] = "Small";
-    Size[Size["Medium"] = 2] = "Medium";
-    Size[Size["Large"] = 3] = "Large";
-})(Size || (Size = {}));
-;
-let mySize = Size.Medium;
-console.log(mySize);
+let employee = {
+    id: 1,
+    name: "Mosh",
+    retire: (date) => {
+        console.log(date);
+    }
+};
 //# sourceMappingURL=index.js.map
